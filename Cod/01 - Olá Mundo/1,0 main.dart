9eff9d71582371f42ab790f66bb06ee752716e97
OsniFilipo <osnifilipo@gmail.com>
2022-06-03 18:53:30 -0300

    1

---
+++ Cod/01 - Olá Mundo/1,0 main.dart
@@ -0,0 +1,10 @@
+// Programa que imprime "Olá Mundo" na tela.
+// Primeiros código feito usando, o dart em um projeto.
+// void main é uma função .
+// Tem que no final o ponto e virgula .
+
+void main() {
+
+    print('Olá Mundo');
+
+}
---
+++ Cod/01 - Olá Mundo/2,0 main.dart
@@ -0,0 +1,8 @@
+// O programa executa o print que vai vim primeiro e depois exexuta o proximo print.
+void main() {
+
+    print('Olá');
+
+    print('Mundo');
+
+}
