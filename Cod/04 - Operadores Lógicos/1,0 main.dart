16b6bef799978f8501019edda0dbc9f9f1788cb1
OsniFilipo <osnifilipo@gmail.com>
2022-06-09 18:40:25 -0300

    04

---
+++ Cod/04 - Operadores Lógicos/1,0 main.dart
@@ -0,0 +1,13 @@
+// if é usado para fazer uma condição, se a condição for verdadeira, ele executa o bloco de código.
+// else é usado para fazer uma condição, se a condição for falsa, ele executa o bloco de código.
+
+void main() {
+
+  int idade = 16;
+if (idade >= 18) {
+  print("Maior de idade");
+} else {
+  print("Menor de idade");
+}
+
+}
